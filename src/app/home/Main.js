274116c00af9a5e1d266ab1be15088c599c7e25a
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-03-29 16:04:42 +0200

    Home Page

---
+++ src/app/home/Main.js
@@ -0,0 +1,28 @@
+import { Btn, Card } from "@/components";
+
+const Main = () => {
+    return ( 
+        <div className="main">
+            <div>
+                <div>
+                    <h1></h1>
+                    <p></p>
+                </div>
+                <div>
+                    <Btn></Btn>
+                    <Btn></Btn>
+                </div>
+                <div>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+}
+ 
+export default Main;

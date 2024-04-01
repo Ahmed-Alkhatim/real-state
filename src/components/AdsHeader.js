ee4df8d43b6e514d9229c1163e89d4425cf70cc1
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-04-01 10:57:09 +0200

    Ads Header

--- src/components/AdsHeader.js
+++ src/components/AdsHeader.js
@@ -1,14 +1,22 @@
 'use client'
+import { useState } from "react";
+import { CloseBtn } from "@/components"
 const AdsHeader = () => {
- 
+
+    const [isDisplayed, setIsDisplayed] = useState(true);
+
     return ( 
-        <div class = "advertiser">
-            {/* <img src = '/ads-bg.png'/> */}
-            <p className = "text-center">
-                <span className="mx-2">✨Discover Your Dream Property with Estatein</span>
-                <a href="">learn more</a>
-            </p>
-        </div>
+        <>
+        { isDisplayed && 
+            <div className="relative bg-ads bg-cover bg-center h-[68px] border-b border-gray-3 ">
+                <div className = "absolute top-1/2 -translate-y-1/2 right-[60px]">
+                    <CloseBtn onClose={() => { setIsDisplayed(false) }}/>
+                </div>
+                <div className = " text-medium text-center line leading-[68px] text-[#FFF]">
+                    ✨Discover Your Dream Property with Estatein
+                </div>
+            </div>}
+        </> 
     );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,13 @@
 'use client'
 import { useState } from "react";
-import { Btn, CloseBtn } from "@/components"
-
+import { Btn } from "@/components"
+import AdsHeader from "./AdsHeader"
 const Header = () => {
-    const [isDisplayed, setIsDisplayed] = useState(true);
+
+
     return(
         <div className="container">
-           { isDisplayed && <div className="relative bg-ads bg-cover bg-center h-[68px] border-b border-gray-3 ">
-                <div className = "absolute top-1/2 -translate-y-1/2 right-[60px]">
-                    <CloseBtn onClose={() => { setIsDisplayed(false) }}/>
-                </div>
-                <div className = " text-medium text-center line leading-[68px] text-[#FFF]">
-                    ✨Discover Your Dream Property with Estatein
-                </div>
-               
-            </div>}
-            
-            <div><Btn>Contact Us</Btn></div>
+            <AdsHeader />
         </div>
     )
 }

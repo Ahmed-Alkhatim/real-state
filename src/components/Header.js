fc296e3a4f684d055ca24c456b03f144065532ee
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-04-01 15:32:26 +0200

    Set AdsHeader in Header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 'use client'
 import { useState } from "react";
-import { Btn } from "@/components"
-import AdsHeader from "./AdsHeader"
+import { Btn, CloseBtn } from "@/components"
+
+
 const Header = () => {
-
-
     return(
         <>
             <AdsHeader />
@@ -12,6 +11,27 @@ const Header = () => {
         </>
     )
 }
+
+
+const AdsHeader = () => {
+
+    const [isDisplayed, setIsDisplayed] = useState(true);
+
+    return ( 
+        <>
+        { isDisplayed && 
+            <div className="relative bg-ads bg-cover bg-center h-[49px] border-b border-gray-3 ">
+                <div className = "absolute top-1/2 -translate-y-1/2 right-[60px]">
+                    <CloseBtn onClose={() => { setIsDisplayed(false) }}/>
+                </div>
+                <div className = " text-medium text-center line leading-[49px] text-[#FFF]">
+                    ✨Discover Your Dream Property with Estatein
+                </div>
+            </div>}
+        </> 
+    );
+}
+
 
 
 const Navigators = () => {
